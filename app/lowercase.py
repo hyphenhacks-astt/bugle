213855628a34a3b4b01db7ad751a68a989141462
Angel <s_angel.mendozapablo@ousd.org>
2018-10-21 03:28:20 -0700

    did the html

--- app/lowercase.py
+++ app/lowercase.py
@@ -1,65 +1,65 @@
-h = input('letter?')
+#h = input('letter?')
 def asciLower(h):
-elif(h=='a'):
-	return('01100001'):
-elif(h=='b'):
-	return('01100010')
-elif(h=='c')
-    return('01100011')
-elif(h=='d')
-    return('01100100')
-elif(h=='e')
-    return('01100101')
-elif(h=='f')
-     return('01100110')
-elif(h=='g')
-     return('01100111') 
-elif(h=='h')   
-     return('01101000')
-elif(h=='i')
-     return('01101001')
-elif(h=='j')
-     return('01101010')
-elif(h=='k')
-     return('01101011')
-elif(h=='l')
-     return('01101100')
-elif(h=='m')
-     return('01101101')
-elif(h=='n')
-     return('01101110')
-elif(h=='o')
-     return('01101111')  
-elif(h=='p')   
-     return('01110000')
-elif(h=='q')
-     return('01110001')
-elif(h=='r')
-     return('01110010')
-elif(h=='s')
-     return('01110011')
-elif(h=='t')
-     return('01110100')
-elif(h=='u')
-     return('01110101')
-elif(h=='v')
-     return('01110110')
-elif(h=='w')
-     return('01110111')
-elif(h=='x')
-     return('01111000')
-elif(h=='Y')
-     return('01111001')
-elif(h=='z')
-     return('01111010')
-elif(h=='{')
-     return('01111010')
-elif(h=='|')
-     return('01111100')
-elif(h=='}')
-     return('01111101')
-elif(h=='~')
-     return('01111110')
+	if(h=='a'):
+		return('01100001'):
+	elif(h=='b'):
+		return('01100010')
+	elif(h=='c'):
+	    return('01100011')
+	elif(h=='d'):
+	    return('01100100')
+	elif(h=='e'):
+	    return('01100101')
+	elif(h=='f'):
+	     return('01100110')
+	elif(h=='g'):
+	     return('01100111') 
+	elif(h=='h'):   
+	     return('01101000')
+	elif(h=='i'):
+	     return('01101001')
+	elif(h=='j'):
+	     return('01101010')
+	elif(h=='k'):
+	     return('01101011')
+	elif(h=='l'):
+	     return('01101100')
+	elif(h=='m'):
+	     return('01101101')
+	elif(h=='n'):
+	     return('01101110')
+	elif(h=='o'):
+	     return('01101111')  
+	elif(h=='p'):   
+	     return('01110000')
+	elif(h=='q'):
+	     return('01110001')
+	elif(h=='r'):
+	     return('01110010')
+	elif(h=='s'):
+	     return('01110011')
+	elif(h=='t'):
+	     return('01110100')
+	elif(h=='u'):
+	     return('01110101')
+	elif(h=='v'):
+	     return('01110110')
+	elif(h=='w'):
+	     return('01110111')
+	elif(h=='x'):
+	     return('01111000')
+	elif(h=='Y'):
+	     return('01111001')
+	elif(h=='z'):
+	     return('01111010')
+	elif(h=='{'):
+	     return('01111010')
+	elif(h=='|'):
+	     return('01111100')
+	elif(h=='}'):
+	     return('01111101')
+	elif(h=='~'):
+	     return('01111110')
 
 
 
